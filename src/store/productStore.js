451f668b89cc8501f33276b2f352f052338d042b
Esraaelsayed <sosoelsayed14@gmail.com>
2024-12-20 06:01:28 +0200

    Update productStore.js

--- src/store/productStore.js
+++ src/store/productStore.js
@@ -1,34 +1,15 @@
 import { defineStore } from "pinia";
-
 import axios from "axios";
-
 import { ref, computed } from "vue";
 
 export const useProductStore = defineStore("useProductStore", () => {
   const products = ref([]);
-
   const newArrivals = ref([]);
-
   const topSelling = ref([]);
-
   const productLike = ref([]);
-
   const input = ref("");
 
-  const loadProduct = computed(() => {
-    return products.value.length > 0;
-  });
-
-  const fetch_products = async () => {
-    try {
-      await axios.get(`http://localhost:3000/product`).then((response) => {
-        // console.log(response.data)
-        products.value = response.data;
-      });
-    } catch (error) {
-      console.error("Error fetching products:", error.message);
-    }
-  };
+  const loadProduct = computed(() => products.value.length > 0);
 
   const fetch_products_limit = async () => {
     try {
@@ -39,15 +20,16 @@ export const useProductStore = defineStore("useProductStore", () => {
         productLike.value = response.data.slice(8, 12);
       }
     } catch (error) {
-      if (error.response) {
-        console.log(error.response.data);
-        console.log(error.response.status);
-        console.log(error.response.headers);
-      } else if (error.request) {
-        console.log(error.request);
-      } else {
-        console.log("Error", error.message);
-      }
+      handleError(error);
+    }
+  };
+
+  const fetch_products = async () => {
+    try {
+      const response = await axios.get("http://localhost:3000/product");
+      products.value = response.data;
+    } catch (error) {
+      handleError(error);
     }
   };
 
@@ -60,15 +42,19 @@ export const useProductStore = defineStore("useProductStore", () => {
         products.value = response.data;
       }
     } catch (error) {
-      if (error.response) {
-        console.log(error.response.data);
-        console.log(error.response.status);
-        console.log(error.response.headers);
-      } else if (error.request) {
-        console.log(error.request);
-      } else {
-        console.log("Error", error.message);
-      }
+      handleError(error);
+    }
+  };
+
+  const handleError = (error) => {
+    if (error.response) {
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      console.log(error.request);
+    } else {
+      console.log("Error", error.message);
     }
   };
 
@@ -76,9 +62,7 @@ export const useProductStore = defineStore("useProductStore", () => {
     if (input.value.length < 3) return products.value;
 
     return products.value.filter((product) => {
-      if (
-        product.title.toLowerCase().includes(input.value.toLowerCase()) == false
-      ) {
+      if (product.name.toLowerCase().includes(input.value.toLowerCase()) === false) {
         return false;
       } else {
         return product.title.toLowerCase().includes(input.value.toLowerCase());
@@ -99,3 +83,4 @@ export const useProductStore = defineStore("useProductStore", () => {
     searchProduct,
   };
 });
+
